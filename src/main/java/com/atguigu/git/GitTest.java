4ee4de08c4d6b44449a236e30d7774c9d9ce1e95
Mao <b9e3ddda@163.com>
2021-08-03 14:32:46 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello git8888");
         System.out.println("hello hot-fix");
+        System.out.println("push hello Mao");
     }
 }
