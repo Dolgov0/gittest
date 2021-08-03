8492c9320513c755c7ddc3af1144ae812a97dfda
Mao <b9e3ddda@163.com>
2021-08-03 14:10:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Mao
+ * @create 2021-08-03 下午 2:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
